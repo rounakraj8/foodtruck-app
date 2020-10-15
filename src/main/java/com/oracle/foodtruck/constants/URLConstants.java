4c4c65ce33b4900aa9b4227d6449942c50db04df
rounakr <roraj@castlighthealth.com>
2020-10-15 10:48:01 +0530

    add URL constants

---
+++ src/main/java/com/oracle/foodtruck/constants/URLConstants.java
@@ -0,0 +1,8 @@
+package com.oracle.foodtruck.constants;
+
+public class URLConstants {
+
+  public static final String BASE_URL = "https://data.sfgov.org/resource/jjew-r69b.json";
+  public static final String DAY_ORDER_REQUEST_PARAM = "?dayorder=%s";
+
+}
