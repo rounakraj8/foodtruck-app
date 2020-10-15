0c27b8445d8ba604972d4a67c7795ba2f3cfecbb
rounakr <roraj@castlighthealth.com>
2020-10-15 11:29:14 +0530

    create instance and call method

---
+++ src/main/java/com/oracle/foodtruck/ApplicationRunner.java
@@ -0,0 +1,15 @@
+package com.oracle.foodtruck;
+
+import com.oracle.foodtruck.service.FoodTruckFinderService;
+import com.oracle.foodtruck.service.FoodTruckFinderServiceImpl;
+import java.time.LocalDateTime;
+
+public class ApplicationRunner {
+
+  public static void main(String[] args) {
+    System.out.println("Welcome to the FoodTruck App!\n\n");
+    FoodTruckFinderService foodTruckFinderService = new FoodTruckFinderServiceImpl(
+        LocalDateTime.now());
+    foodTruckFinderService.findFoodTrucksNow();
+  }
+}
